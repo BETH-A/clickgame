c958dc1a3821351671a8e43d179861611111ab76
BETH-A <elizabeth78@me.com>
2018-06-16 21:30:15 -0400

    working app

---
+++ clickgame/src/components/Title/Title.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Title.css";
+
+const that = {
+  primary: {
+    color: "#2f77c4",
+    textShadow: "3px 3px 5px #0b395e",
+    fontSize: "3.0rem",
+    fontWeight: "bold",
+
+  }
+};
+const Title = props => (
+  <div>
+    <div className="jumbotron.transparent jumbotron-fluid">
+      <div className="title">
+        <h1 style={that.primary}>Click on an image to earn points, {props.children}</h1>
+        <h1 style={that.primary}>but don't click on any more than once!{props.children}</h1>
+        <h2>
+          Score: {props.curScore}
+          <tr/>
+          Highscore: {props.topScore}
+        </h2>
+      </div>
+    </div>
+  </div>
+);
+
+export default Title;
