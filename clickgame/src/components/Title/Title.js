70a834341fca691226e511dbeda11521aa3e316d
BETH-A <elizabeth78@me.com>
2018-06-20 20:25:47 -0400

    additional styling

--- clickgame/src/components/Title/Title.js
+++ clickgame/src/components/Title/Title.js
@@ -7,17 +7,18 @@ const that = {
     textShadow: "3px 3px 5px #0b395e",
     fontSize: "3.0rem",
     fontWeight: "bold",
+    paddingTop: "2.0rem" 
   }
 };
 const Title = props => (
   <div>
     <div className="jumbotron.transparent jumbotron-fluid">
       <div className="title">
-        <h1 style={that.primary}>Click on an image to earn points, {props.children}</h1>
-        <h1 style={that.primary}>but don't click on any more than once!{props.children}</h1>
+        <h1 style={that.primary}>Tropical Memory Click Game {props.children}</h1>
+        <h2 style={that.curScore}>Click on an image to earn points, but don't click on any more than once!{props.children}</h2>
         <h2>
-          Score: {props.curScore}
-          <tr/>
+          Score: {props.curScore} | 
+                      
           Highscore: {props.topScore}
         </h2>
       </div>
